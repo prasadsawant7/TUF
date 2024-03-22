f57b521bf29a45b97c813523b4ac70257ea29b32
prasadsawant7 <prasadsawant7git@gmail.com>
2024-03-22 17:44:47 +0530

    --fix removed unused icon

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,6 @@ import SubmissionForm from "@/components/custom/form";
 function Home() {
   return (
     <div className="mx-auto flex max-w-[1400px] flex-col items-start justify-start gap-10 px-4 py-10">
-      {/* <h1 className="text-2xl font-medium">Submit your code</h1> */}
       <SubmissionForm />
     </div>
   );
